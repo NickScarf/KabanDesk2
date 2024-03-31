885b64d3101f052061671f5414971c60b9758a36
Pavlik50599 <pavel.melnichenko.02@mail.ru>
2024-03-31 23:13:17 +0700

    front commit2

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-///style={{backgroundcolor: '#000000'}}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div class="title">KabanDesk</div>
-        <div class='btn-create-container'>
-          <button type="button" class="btn btn-outline-light" style={{top:'100px'}}>Create</button>
-        </div>
-        <div class = 'btn-exit-container'>
-        <button type="button" class="btn btn-outline-danger" style={{top:'100px'}}>Exit</button>
-        </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-
-
-
-      <div>Giosidog </div>
     </div>
   );
 }
